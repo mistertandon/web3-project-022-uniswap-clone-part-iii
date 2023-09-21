16da454d06278835a8aee7436c56accd35808dc0
mistertandon <enggparveshtandon@gmail.com>
2023-09-21 22:45:17 +0530

    feat: Deploy following ERC20 tokens 1. Parvesh 2. Payal  and Jiyanshi
     Fix Contract name for Payala and Jiyanshi ERC20 tokens

--- contracts/Payal.sol
+++ contracts/Payal.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Parvesh is ERC20, Ownable {
+contract Payal is ERC20, Ownable {
     constructor() ERC20("Payal", "PHLA") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
